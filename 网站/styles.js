c6a8ddcae9a8b1fbeeb84fa4b3a6bb4c5e72287a
boundless-y <718676673@qq.com>
2025-05-17 22:20:45 +0800

    Add files via upload

---
+++ 网站/styles.js
@@ -0,0 +1,25 @@
+
+    /*L2Dwidget.init({
+        "model": {
+　　　　　　　//jsonpath控制显示那个小萝莉模型，
+			//(切换模型需要改动)
+//				"https://unpkg.com/(live2d-widget-model-koharu)@1.0.5/assets/(koharu).model.json"
+            jsonPath: "https://unpkg.com/live2d-widget-model-tororo@1.0.5/assets/tororo.model.json",
+            "scale": 1
+        },
+        "display": {
+            "position": "right", //看板娘的表现位置
+            "width": 150,  //小萝莉的宽度
+            "height": 200, //小萝莉的高度
+            "hOffset": 0,
+            "vOffset": -20
+        },
+        "mobile": {
+            "show": true,
+            "scale": 0.5
+        },
+        "react": {
+            "opacityDefault": 0.7,
+            "opacityOnHover": 0.2
+        }
+    });*/
